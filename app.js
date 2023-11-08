80f3fae363cad2f65794d63292dce64a3a83830d
Dhrygon <ByDhrygon@gmail.com>
2023-11-07 21:23:50 -0300

    Modificacion para Servidor de app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+const PORT= process.env.PORT || 3000;
 
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('Servidor funcionando');
 });
 
